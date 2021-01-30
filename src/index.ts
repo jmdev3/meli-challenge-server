de3f2af76acd2b9cb56fde2af9f267ffdae880f9
Juan Manuel <juan.villarraza@gmail.com>
2021-01-30 18:18:40 -0300

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Application } from 'express';
 import queryItems from './controllers';
+import cors from "cors";
 
 require('dotenv').config();
 
 // Boot express
 const app: Application = express();
 const port = process.env.PORT;
+
+app.use(cors());
 
 // Application routing
 app.use('/api', queryItems);
